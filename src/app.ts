4744ced075eedb065b21aae2801759c7a2d45039
giantstar624 <brightcn624@gmail.com>
2024-08-11 16:52:38 -0700

    change

--- src/app.ts
+++ src/app.ts
@@ -3,33 +3,62 @@ import axios from "axios"
 import path from "path"
 import cors from "cors"
 import moment from "moment"
-
+import os from 'os';
+interface Status {
+  [ip: string]: boolean;
+}
+const networkInterfaces = os.networkInterfaces();
+const interfaceName = 'Ethernet';
+let myIP = "";
+if (networkInterfaces[interfaceName]) {
+  for (const net of networkInterfaces[interfaceName]!) {
+    if (net.family === 'IPv4' && !net.internal) {
+      console.log(`Primary IP Address: ${net.address}`);
+      myIP = net.address;
+      break;
+    }
+  }
+} else {
+  console.log(`No such interface: ${interfaceName}`);
+}
 const rdpInfo = {
-  "server": "34.224.109.221",
   "user": "administrator",
   "password": "mO29pXzn%B2Kf(hwoBM$vOXbpDQ3hoWw"
 }
 //L4I(wYd)lIzqTJAEmObrL2x!GP3eUvo9
 const app = express();
-
-const ec2BackendUrl = process.env.EC2_BACKEND_URL;
 const backendIp = process.env.BACKEND_IP;
-
+//ip:available
+const status: Status = { "34.224.109.221": false };
 app.use(cors());
 
 app.get("/launch", async (req: Request, res: Response) => {
-  axios.get(`${ec2BackendUrl}/launch`, { params: { id: moment().format('YYYY-MM-DD-HH-mm-ss') } });
-  const passwordHash = (await axios.get(`http://${backendIp}/Myrtille/GetHash.aspx`, { params: { password: rdpInfo.password } })).data;
-  res.redirect(encodeURI(`http://${backendIp}/Myrtille/?__EVENTTARGET=&__EVENTARGUMENT=&server=${rdpInfo.server}&user=${rdpInfo.user}&passwordHash=${passwordHash}&connect=Connect`))
+  for (const ip in status) {
+    if (status[ip] == true) {
+      status[ip] = false;
+      axios.get(`${myIP}/launch`, { params: { id: moment().format('YYYY-MM-DD-HH-mm-ss') } });
+      const passwordHash = (await axios.get(`http://${ip}:8001/Myrtille/GetHash.aspx`, { params: { password: rdpInfo.password } })).data;
+      res.send({ launched: true, url: encodeURI(`http://${ip}:8001/Myrtille/?__EVENTTARGET=&__EVENTARGUMENT=&server=${ip}&user=${rdpInfo.user}&passwordHash=${passwordHash}&connect=Connect`) });
+      return;
+    }
+  }
+  res.send({ launched: false });
 });
 
 app.get("/logs", async (req: Request, res: Response) => {
-  res.send((await axios.get(`${ec2BackendUrl}/logs`, { params: { id: req.query.id } })).data);
+  const result = [];
+  for (const ip in status) {
+    result.push(...(await axios.get(`http://${ip}:8001/logs`, { params: { id: req.query.id } })).data);
+  }
+  res.send(result);
 });
 
 app.get("/ids", async (req: Request, res: Response) => {
-  const { data } = await axios.get(`${ec2BackendUrl}/ids`);
-  res.send(data);
+  const result = [];
+  for (const ip in status) {
+    result.push(...(await axios.get(`http://${ip}:8001/ids`, { params: { id: req.query.id } })).data);
+  }
+  res.send(result);
 })
 
 app.listen(8001, () => {
